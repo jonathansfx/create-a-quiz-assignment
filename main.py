4de1c20e759ebc81db5adf88e2f5db1b4f99c617
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-03-15 11:22:31 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# welcome message
+print("WELCOME TO THE PORSCHE QUIZ")
+
+# score
+score = 0
+
+# q1
+answer1 = input("the porsche turbo s is an everyday car (t or f)")
+if answer1.lower() == "t":
+    print("correct")
+else:
+    print("incorrect")
+
+# q2
+answer2 = input("")
