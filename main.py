3b29ce43dbd79b7c25fc259fc5dc518d438157d9
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-03-16 10:25:37 -0600

    sadfsadf

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 # welcome message
-print("WELCOME TO THE PORSCHE QUIZ")
+print("WELCOME TO THE MATH QUIZ")
 
 # score
 score = 0
 
 # q1
-answer1 = input("the porsche turbo s is an everyday car (t or f)")
-if answer1.lower() == "t":
+answer1 = input("\n1 + 1 = ")
+if answer1 == "2":
     print("correct")
+    score += 1
 else:
     print("incorrect")
 
 # q2
-answer2 = input("")
+answer2 = input("2 + 2 = ")
+if answer2 == "4":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
+
+# q3
+answer3 = input("2 x 1 = ")
+if answer3 == "2":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
+
+# q4
+answer4 = input("3 x 2 + 2 = ")
+if answer4 == "8":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
+
+# print score + message
+percent = score / 4 * 100
+
+print("SCORE:", score, "/ 4", percent,)
+if score <= 2:
+    print("you need to study more")
+elif score > 2:
+    print("good job")
