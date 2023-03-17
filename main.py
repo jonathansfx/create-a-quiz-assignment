72c9c08f33b1cbf953ab2721c91c4f796fb9710b
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-03-17 10:40:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ print("WELCOME TO THE MATH QUIZ")
 score = 0
 
 # q1
-answer1 = input("\n1 + 1 = ")
+print("\n1. 1 + 1 = __ ?")
+answer1 = input("Answer: ")
 if answer1 == "2":
     print("correct")
     score += 1
@@ -13,7 +14,8 @@ else:
     print("incorrect")
 
 # q2
-answer2 = input("2 + 2 = ")
+print("\n2. 2 + 2 = __ ?")
+answer2 = input("Answer: ")
 if answer2 == "4":
     print("correct")
     score += 1
@@ -21,7 +23,8 @@ else:
     print("incorrect")
 
 # q3
-answer3 = input("2 x 1 = ")
+print("\n3. 2 x 1 = __ ?")
+answer3 = input("Answer: ")
 if answer3 == "2":
     print("correct")
     score += 1
@@ -29,8 +32,9 @@ else:
     print("incorrect")
 
 # q4
-answer4 = input("3 x 2 + 2 = ")
-if answer4 == "8":
+print("\n4. 3^2 = __ ?")
+answer4 = input("Answer: ")
+if answer4 == "9":
     print("correct")
     score += 1
 else:
@@ -39,7 +43,7 @@ else:
 # print score + message
 percent = score / 4 * 100
 
-print("SCORE:", score, "/ 4", percent,)
+print("SCORE:", score, "/ 4", "(", + percent, "%)")
 if score <= 2:
     print("you need to study more")
 elif score > 2:
